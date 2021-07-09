a1380ef37f1c3e9816afe972147006c51124072d
Marie-co <43325419+Marie-co@users.noreply.github.com>
2021-07-09 14:19:25 +0200

    nw version

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const navMenu =document.getElementById('nav-menu');
 const navToggle =document.getElementById('nav-toggle');
 const navClose =document.getElementById('nav-close');
+const navLink = document.querySelectorAll('.nav-link') ;
 
 
 // MENU SHOW
@@ -16,4 +17,22 @@ if(navToggle){
         })
     }
 }
+
+// REMOVE MENU MOBILE
+
 console.log('hello');
+
+function linkAction(){
+    const navMenu=document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n=> n.addEventListener('click', linkAction))
+
+// CHANGE BACKGROUND HEADER
+
+function scrollHeader(){
+    const header= document.getElementById('header')
+
+    if(this.scrollY >= 100) header.classList.add('scroll-header');else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
