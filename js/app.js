8c7fcd656e3ab5ec73b7b705a64b3ef5d47e2fad
Marie-co <43325419+Marie-co@users.noreply.github.com>
2021-07-10 21:15:27 +0200

    dark mode en cours

--- js/app.js
+++ js/app.js
@@ -36,3 +36,77 @@ function scrollHeader(){
     if(this.scrollY >= 100) header.classList.add('scroll-header');else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+// SWIPPER
+var swiper = new Swiper(".discover__container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop:true,
+    spaceBetween:32,
+    coverflowEffect: {
+      rotate: 50,
+    
+  
+    },
+  });
+
+//   VIDEO
+const videoFile=document.getElementById('video-file');
+const videoButton= document.getElementById('video-button');
+const videoIcon= document.getElementById('video-icon');
+function playPause(){
+
+    if(videoFile.paused){
+        videoFile.play()
+        videoIcon.classList.add("ri-pause-fill")
+        videoIcon.classList.remove("ri-play-line")
+    }
+    else{
+        videoFile.pause()
+        videoIcon.classList.remove("ri-pause-fill")
+        videoIcon.classList.add("ri-play-line")
+    
+    }
+
+
+}
+
+videoButton.addEventListener('click',playPause)
+function finalVideo(){
+    videoIcon.classList.remove("ri-pause-fill")
+    videoIcon.classList.add("ri-play-line")
+
+}
+videoFile.addEventListener("ended",finalVideo)
+
+
+// SCROLL UP
+function scrollUp(){
+    const scrollUp= document.getElementById('scroll-up')
+    if(this.scrollY >= 200)scrollUp.classList.add('show-scroll')
+    window.addEventListener('scroll',scrollUp)
+}
+
+// SCROLL SECTION ACTIVE LINK
+const sections= document.querySelectorAll('section[id]')
+function scrollActive(){
+    const scrollY = window.pageYOffset
+    
+    sections.forEach(current=>{
+        const sectionHeight =current.offsetHeight
+        const sectionTop= current.offsetTop -50;
+        sectionId =current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop +sectionHeight)
+
+        {
+            document.querySelector('.nav__menu a[href*=' +sectionId +']').classList.add('active-link')}else{
+                document.querySelector('.nav__menu a[href*=' +sectionId +']').classList.remove('active-link')
+            }
+            
+        
+    })
+}
+window.addEventListener('scroll',scrollActive)
